6d63a4b6489f98c67576b44b7b46b4095989b0dc
Kelly <kellyguo123@hotmail.com>
2015-10-18 14:08:30 -0400

    Update Gummies.java

--- Gummies.java
+++ Gummies.java
@@ -1,5 +1,5 @@
 //****************************************************************************************
-//Gummies Class (Assignment 2)
+//Gummies Class
 //Kelly Guo
 //Date:November 22, 2012
 //Java, Eclipse 3.4
